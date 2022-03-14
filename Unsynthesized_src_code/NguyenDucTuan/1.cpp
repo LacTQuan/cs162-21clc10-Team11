541777f3a464e97cf1e554e0b4365e2e96126f0a
tuannguyenduc3002 <tuannguyenduc3002@gmail.com>
2022-03-14 22:19:34 +0700

    create year & class

--- Unsynthesized_src_code/NguyenDucTuan/1.cpp
+++ Unsynthesized_src_code/NguyenDucTuan/1.cpp
@@ -25,3 +25,37 @@ struct Year{
     Year* pNext_Year;
     Semester* pSemester;
 };
+void Create_Year(Year* &year){
+    string name_year;
+    cout<<"Import academic year (ex: 2021) ";
+    cin >> name_year;
+    Year* pCur_Year = year;
+
+    Year* Temp_Year = new Year;
+    Temp_Year->name = name_year;
+    Temp_Year->pNext_Year = NULL;
+    Temp_Year->pClass = NULL;
+    Temp_Year->pSemester = nullptr;
+
+    if(year == NULL) year = Temp_Year;
+    else{
+        while(pCur_Year->pNext_Year != NULL) pCur_Year = pCur_Year->pNext_Year;
+        pCur_Year->pNext_Year = Temp_Year;
+    }
+}
+void Create_Class(Year* &year){
+    string name_class;
+    cout<<"Import class (ex:21CLC10) ";
+    cin >> name_class;
+    Class* pCur_Class = year->pClass;
+
+    Class* Temp_Class = new Class;
+    Temp_Class->name = name_class;
+    Temp_Class->pNext_Class = NULL;
+
+    if(year->pClass == NULL) year->pClass = Temp_Class;
+    else{
+        while(pCur_Class->pNext_Class != NULL) pCur_Class = pCur_Class->pNext_Class;
+        pCur_Class->pNext_Class = Temp_Class;
+    }
+}
