2ca83eb950aa630b050c5d74572df0225a8b7df2
tuannguyenduc3002 <tuannguyenduc3002@gmail.com>
2022-03-15 10:03:39 +0700

    create student

--- Unsynthesized_src_code/NguyenDucTuan/1.cpp
+++ Unsynthesized_src_code/NguyenDucTuan/1.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<cstring>
 #include<fstream>
+#include<iomanip>
 using namespace std;
 //right
 struct Student{
@@ -58,4 +59,31 @@ void Create_Class(Year* &year){
         while(pCur_Class->pNext_Class != NULL) pCur_Class = pCur_Class->pNext_Class;
         pCur_Class->pNext_Class = Temp_Class;
     }
+}
+void Create_Student(Class *classes){
+
+    //No, StudentID, FirstName, LastName, Gender, Date_of_Birth, SocialID
+    
+    ifstream filein("profile.csv");
+    while(filein.eof() == false){
+        Student* pCur_Student = classes->pStudent;
+        Student* Temp_Student = new Student;
+        cout<<"Import student: "<<endl;
+        cout<<"No ";getline(filein,Temp_Student->No,',');cout<<Temp_Student->No<<" | ";
+        cout<<"StudentID ";getline(filein,Temp_Student->StudentID,',');cout<<Temp_Student->StudentID<<" | ";
+        cout<<"FirstName ";getline(filein,Temp_Student->FirstName,',');cout<< " " << setw(20) << left << Temp_Student->FirstName;
+        cout<<"LastName ";getline(filein,Temp_Student->LastName,',');cout<< " " << setw(15) << left << Temp_Student->LastName<<" | ";
+        cout<<"Gender ";getline(filein,Temp_Student->Gender,',');cout<<setw(7) << left << Temp_Student->Gender<<" | ";
+        cout<<"Date_of_Birth ";getline(filein,Temp_Student->Date_of_Birth,',');cout<<Temp_Student->Date_of_Birth<<" | ";
+        cout<<"SocialID ";getline(filein,Temp_Student->SocialID,'\n');cout<<Temp_Student->SocialID<<endl;
+        Temp_Student->pNext_Student = NULL;
+
+        if(classes->pStudent == NULL) classes->pStudent = Temp_Student;
+        else{
+            while(pCur_Student->pNext_Student != NULL) pCur_Student = pCur_Student->pNext_Student;
+            pCur_Student->pNext_Student = Temp_Student;
+        }    
+    }
+    
+    filein.close();
 }
