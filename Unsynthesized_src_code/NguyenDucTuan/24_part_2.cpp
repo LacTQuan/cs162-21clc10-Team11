7a00f8d07d178de88f00171450218f22f39c22cc
tuannguyenduc3002 <tuannguyenduc3002@gmail.com>
2022-04-04 10:57:29 +0700

    completed_task_24

---
+++ Unsynthesized_src_code/NguyenDucTuan/24_part_2.cpp
@@ -0,0 +1,55 @@
+//24
+void update_student_result(Year* &year, Semester* &sem){
+    //view all course
+    //Print_Course(sem);
+    //find course
+    string course_name;
+    cout<<"Course you want to choose "; cin>>course_name;
+    Course* course_update = find_course_in_many_subjects(sem,course_name);
+    if(course_update == NULL){
+        cout<<"invalid course_update"<<endl;
+        //cin.ignore();
+        //getchar();
+        return;
+    }
+    system("cls");
+    //view student in a course
+    View_Score_Board(course_update);
+    // find student
+
+    string studentID;
+    cout<<"StudentID "; cin>>studentID;
+
+    Student* student = find_student_in_many_classes(year, studentID);
+    if(student == NULL){
+        cout<<"invalid student"<<endl;
+        //cin.ignore();
+        //getchar();
+        return;
+    }
+    Student_Course* student_cour = find_student_in_a_course(course_update,student->StudentID);
+    
+    cout<<"Update score\n";
+    cout<<"Midterm ";cin>>student_cour->mark.Midterm;
+    cout<<"Final ";cin>>student_cour->mark.Final;
+    cout<<"Other ";cin>>student_cour->mark.Other;
+    double a,b,c;
+    a = student_cour->mark.Midterm;
+    b = student_cour->mark.Final;
+    c = student_cour->mark.Other;
+    student_cour->mark.Total = int(((a + b* 2)/3 + c)* 10 )/10.0;// lam tron 1 chu so thap phan
+
+
+    Cur_Course* cur_cour = student->pCur_Cour;
+    while(cur_cour != NULL){
+        if(cur_cour->Course_Name != course_update->Course_Name) cur_cour = cur_cour->pNext_Cur_Cour;
+        else break;
+    }
+    // copy student_course score into cur_course student
+    cur_cour->mark.Midterm = student_cour->mark.Midterm;
+    cur_cour->mark.Final = student_cour->mark.Final;
+    cur_cour->mark.Other = student_cour->mark.Other;
+    cur_cour->mark.Total = student_cour->mark.Total;
+
+    cout << "Result updated.\n";
+}
